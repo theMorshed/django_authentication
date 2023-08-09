e9289a38b803ca6edf52796be75ccc90686621e9
theMorshed <themorshedctg@gmail.com>
2023-08-09 22:48:07 +0600

    create user registration form using django default User and UserCreationForm

--- authentication/views.py
+++ authentication/views.py
@@ -1,7 +1,16 @@
 from django.shortcuts import render
 from authentication.forms import UserRegistrationForm
+from django.contrib import messages
 
 # Create your views here.
 def home(request):
-    register_form = UserRegistrationForm(request.POST)
+    if request.method == 'POST':
+        register_form = UserRegistrationForm(request.POST)
+        if register_form.is_valid():        
+            register_form.save()
+            messages.success(request, 'Account created successfully')
+            # print(register_form.cleaned_data)
+    else:
+        register_form = UserRegistrationForm()
+
     return render(request, 'home.html', {'register_form': register_form})
